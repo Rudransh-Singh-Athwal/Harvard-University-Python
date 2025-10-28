b4798d2442812a3f731b1fcd2689c1d0c4055c4f
Rudransh Singh Athwal <rsathwal04@gmail.com>
2025-10-28 23:56:59 +0530

    pset_0/faces.py

---
+++ Problem Set 0/faces.py
@@ -0,0 +1,11 @@
+def convert(text):
+  text = text.replace(":)", "🙂")
+  text = text.replace(":(", "🙁")
+  
+  return text
+
+def main():
+  x = input("Enter the text: ")
+  print(convert(x))
+  
+main()
